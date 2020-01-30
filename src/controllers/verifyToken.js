6c31a29fc312acc2cdb0243ec8de50202a94ce01
Pedro Ruiz <ruizhidalgopedro@gmail.com>
2020-01-30 13:22:07 +0100

    src/controllers/verifyToken.js

---
+++ src/controllers/verifyToken.js
@@ -0,0 +1,30 @@
+const jwt = require('jsonwebtoken')
+
+function verifyToken(req, res, next)
+{
+  const token= req.headers['x-api-key']
+  if( ! token )
+  {
+    return res.status(401).json({
+      auth: false,
+      message: 'No token provided'
+    })
+  }
+
+  try
+  {
+    var decoded = jwt.verify(token,process.env.SECRET)
+  }
+  catch( err )
+  {
+    return res.json({
+      auth: false,
+      message: err.message,
+      expiredAt: err.expiredAt
+    })
+  }
+  req.userId = decoded.id 
+  next()
+}
+
+module.exports = verifyToken
