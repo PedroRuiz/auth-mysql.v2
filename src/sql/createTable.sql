a833c6500c0d05dad043c3b31848cbcb100a552d
Pedro Ruiz <ruizhidalgopedro@gmail.com>
2020-01-31 14:11:55 +0100

    sql for both: applications and apiusers

--- src/sql/createTable.sql
+++ src/sql/createTable.sql
@@ -1,15 +1,35 @@
+/**
+* run this sql in your database
+**/
+
+CREATE TABLE `applications` (
+  `idapplications` int(11) NOT NULL AUTO_INCREMENT,
+  `apikey` char(31) DEFAULT NULL,
+  `name` varchar(255) DEFAULT NULL,
+  `create_time` timestamp NULL DEFAULT current_timestamp(),
+  `end_time` date DEFAULT NULL,
+  `active` tinyint(1) DEFAULT NULL,
+  PRIMARY KEY (`idapplications`),
+  UNIQUE KEY `K_apikey` (`apikey`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8
+
+
 CREATE TABLE `apiusers` (
   `idapiusers` int(11) unsigned NOT NULL AUTO_INCREMENT,
-  `username` varchar(45) NOT NULL,
-  `email` varchar(255) NOT NULL,
-  `password` varchar(80) NOT NULL,
-  `uuid` char(36) NOT NULL,
-  `apikey` char(31) NOT NULL,
+  `applicationkey` int(11) NOT NULL,
+  `username` varchar(45) CHARACTER SET latin1 NOT NULL,
+  `email` varchar(255) CHARACTER SET latin1 NOT NULL,
+  `password` varchar(80) CHARACTER SET latin1 NOT NULL,
+  `uuid` char(36) CHARACTER SET latin1 NOT NULL,
+  `apikey` char(31) CHARACTER SET latin1 NOT NULL,
   `startdate` date NOT NULL,
   `enddate` date DEFAULT NULL,
   `active` smallint(1) NOT NULL DEFAULT 0,
   PRIMARY KEY (`idapiusers`),
-  UNIQUE KEY `unique-email` (`email`),
   UNIQUE KEY `unique-uuid` (`uuid`),
-  UNIQUE KEY `unique-apikey` (`apikey`)
-) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=latin1
+  UNIQUE KEY `unique-apikey` (`apikey`),
+  UNIQUE KEY `unique-app-email` (`email`,`applicationkey`),
+  KEY `fk_apiusers_1_idx` (`applicationkey`),
+  CONSTRAINT `fk_apiusers_1` FOREIGN KEY (`applicationkey`) REFERENCES `applications` (`idapplications`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=78941 DEFAULT CHARSET=utf8
+
